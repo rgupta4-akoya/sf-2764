bc8de0cf55facc18317f1cb38fe38a6e3908430f
Robert Gupta <robert.gupta@gmail.com>
2018-11-14 17:21:26 -0800

    swagger adds

---
+++ src/main/java/io/lewandowski/tutorial/spring/jsonformatting/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package io.lewandowski.tutorial.spring.jsonformatting;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+/*
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.lewandowski.tutorial.spring.jsonformatting"))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Codex Server REST API")
+                .description("Codex Server REST API for Codex Analysis Platform")
+                .version("1.0.0")
+                .license("XXXX")
+                .licenseUrl("https://www.akoyabio.com")
+                .contact(new Contact("Akoya Technical Support", "https://www.akoyabio.com/support", "support@akoyabio.com"))
+                .build();
+    }
+}
+*/
+
