69600e000a1acf1d960e0606ed41609bfa2ac8f8
Ildar Almakaev <ildar.almakaev@akvelon.com>
2018-11-17 00:23:14 +0300

    Add jackson message converter for Swagger config

--- src/main/java/io/lewandowski/tutorial/spring/jsonformatting/SwaggerConfig.java
+++ src/main/java/io/lewandowski/tutorial/spring/jsonformatting/SwaggerConfig.java
@@ -1,11 +1,11 @@
 package io.lewandowski.tutorial.spring.jsonformatting;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -16,16 +16,16 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import static java.time.format.DateTimeFormatter.ofPattern;
+import java.util.List;
 
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
+
+    @Autowired
+    private ObjectMapper serializingObjectMapper;
 
     @Bean
     public Docket productApi() {
@@ -37,6 +37,14 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     }
 
     @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        messageConverter.setObjectMapper(serializingObjectMapper);
+        converters.add(messageConverter);
+        super.configureMessageConverters(converters);
+    }
+
+    @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
@@ -44,6 +52,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
                 .title("Codex Server REST API")
