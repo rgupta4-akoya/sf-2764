0ec8b5beabaa87ea6210be83fbc747749ef1ec9f
Ildar Almakaev <ildar.almakaev@akvelon.com>
2018-12-05 20:33:21 +0300

    Fix serialization for plain texts

--- src/main/java/io/lewandowski/tutorial/spring/jsonformatting/Application.java
+++ src/main/java/io/lewandowski/tutorial/spring/jsonformatting/Application.java
@@ -3,15 +3,12 @@ package io.lewandowski.tutorial.spring.jsonformatting;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
@@ -28,13 +25,11 @@ public class Application {
     @Primary
     public ObjectMapper serializingObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         JavaTimeModule javaTimeModule = new JavaTimeModule();
         javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer());
         javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer());
         objectMapper.registerModule(javaTimeModule);
         return objectMapper;
     }
-
-
 }
--- src/main/java/io/lewandowski/tutorial/spring/jsonformatting/SwaggerConfig.java
+++ src/main/java/io/lewandowski/tutorial/spring/jsonformatting/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -41,6 +42,11 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
         messageConverter.setObjectMapper(serializingObjectMapper);
         converters.add(messageConverter);
+
+        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
+        stringHttpMessageConverter.setWriteAcceptCharset(true);
+        converters.add(stringHttpMessageConverter);
+
         super.configureMessageConverters(converters);
     }
 
